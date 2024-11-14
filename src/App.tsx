c462d9acdb787b1ae9f1732170c743fbfd03bb68
Fiammamuscari <fiammamuscari@gmail.com>
2024-11-14 12:44:30 -0300

    🌈 chore: initial setup Vite w/ Tailwind

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-5xl font-bold bg-gradient-to-r from-pink-500 via-purple-500 to-blue-500 text-transparent bg-clip-text">
+        open source is love
+      </h1>
+    </>
+  );
+}
+
+export default App;
